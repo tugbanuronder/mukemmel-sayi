c5c4484e680de3b345badf9f6fcab2293c6e1793
tugbanuronder <94485340+tugbanuronder@users.noreply.github.com>
2022-08-11 22:29:54 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input= new Scanner(System.in);
+
+        System.out.print("Bir sayı giriniz:");
+        int number=input.nextInt();
+
+        int total=0;
+
+        for(int i=1 ; i<number ; i++){
+
+            if(number % i ==0){
+
+                int carpan=i;
+                total +=carpan;
+            }
+        }
+
+        if(number==total){
+            System.out.println(number + " mükemmel sayıdır.");
+        }
+        else {
+            System.out.println(number + " mükemmel sayı değildir.");
+        }
+    }
+}
